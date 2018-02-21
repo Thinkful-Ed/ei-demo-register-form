1731779ca27077fd44309f0887b75bfeab7713a9
Joe Turner <joe@oampo.co.uk>
2018-02-21 17:08:03 +0000

    Add input component

--- src/register-form.js
+++ src/register-form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {reduxForm, Field} from 'redux-form';
+import Input from './input';
 import {required, notEmpty, email} from './validators';
 
 class RegisterForm extends React.Component {
@@ -14,24 +15,30 @@ class RegisterForm extends React.Component {
                 onSubmit={this.props.handleSubmit(values =>
                     this.onSubmit(values)
                 )}>
-                <label htmlFor="name">Name</label>
                 <Field
-                    component="input"
+                    component={Input}
+                    element="input"
+                    label="Name"
                     type="text"
                     name="name"
                     id="name"
                     validate={[required, notEmpty]}
                 />
-                <label htmlFor="email">Email</label>
                 <Field
-                    component="input"
+                    component={Input}
+                    element="input"
+                    label="Email"
                     type="text"
                     name="email"
                     id="email"
                     validate={[required, notEmpty, email]}
                 />
-                <label htmlFor="experience">Experience</label>
-                <Field component="select" name="experience" id="experiene">
+                <Field
+                    component={Input}
+                    element="select"
+                    label="Experience"
+                    name="experience"
+                    id="experiene">
                     <option value="beginner">Beginner</option>
                     <option value="intermediate">Intermediate</option>
                     <option value="advanced">Advanced</option>
