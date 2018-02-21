264b45409822404ae30baf2586f599d4672fb960
Joe Turner <joe@oampo.co.uk>
2018-02-21 15:39:25 +0000

    Convert form fields to Redux Form Fields

--- src/register-form.js
+++ src/register-form.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-import {reduxForm} from 'redux-form';
+import {reduxForm, Field} from 'redux-form';
 
 class RegisterForm extends React.Component {
     render() {
         return (
             <form>
                 <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
+                <Field component="input" type="text" name="name" id="name" />
                 <label htmlFor="email">Email</label>
-                <input type="text" name="email" id="email" />
+                <Field component="input" type="text" name="email" id="email" />
                 <label htmlFor="experience">Experience</label>
-                <select name="experience" id="experiene">
+                <Field component="select" name="experience" id="experiene">
                     <option value="beginner">Beginner</option>
                     <option value="intermediate">Intermediate</option>
                     <option value="advanced">Advanced</option>
-                </select>
+                </Field>
                 <button>Register interest</button>
             </form>
         );
